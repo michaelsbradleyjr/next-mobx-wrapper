4dfe1bc627fe2b62b6ea5dbb872f8069d6474d80
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2018-06-21 15:35:45 -0500

    rev notes, todo

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ export * from './store';
 // notes, todo
 /* =============================================================================
 
-(+) clean up package.json "scripts" -- some may not be necessary; clarify
-    whether it's really necessary to build w/ babel to import withMobX from
-    pages/ scripts, i.e. next-redux-wrapper imports from '../lib' but maybe can
-    import from '../src'; if can import from '../scr' then revise "start" (and
-    may not need "watch")
-
 (+) use WeakMap with page/app wrappers (pass wrapper to factory's "make" and on
     to StoreHouse instance's "handle"), involving a combo of keys and counters,
     to have a mechanism that expunges store-name keys in a StoreHouse; this
@@ -29,9 +23,6 @@ export * from './store';
     old-page pre/fetch logic issues w.r.t. other pages/app using same key name
     that have also changed though browser isn't navigated to them at that time
 
-(+) revise error messages and warnings using template literals and "dedent";
-    see: https://github.com/dmnd/dedent
-
 (+) StoreHouse "handle" method should be revised so that it subsumes task of
     checking for existing store-name key, and therefore the next steps after it
     is called can proceed to setting/using a value based on handle's return
@@ -41,13 +32,6 @@ export * from './store';
 (+) StoreHouse can be revised (e.g. its "set" and "handle" methods) so that
     it's stricter about the relationships between Store constructors and
     instances and StoreFactory constructors
-
-(+) revise impl of extendsDocument to avoid use of eval
-    see: https://arunoda.me/blog/ssr-and-server-only-modules
-    maybe can just put the package name ('next/document') into a variable and
-    invoke require via a function that would be called w/ that variable, so
-    that webpack can't analyze it... need to test using the "webpack analyzer"
-    mentioned in the blog post (link above)
 
 (+) formal tests
 
